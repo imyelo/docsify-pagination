d1c96dab84cd4a584d7a25757e9b351861d0765b
yelo <zhihuzeye@gmail.com>
2017-12-11 23:38:29 +0800

    fix: group pages

--- rollup.config.base.js
+++ rollup.config.base.js
@@ -1,12 +1,21 @@
 import commonjs from 'rollup-plugin-commonjs'
 import nodejs from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
+import alias from 'rollup-plugin-alias'
+import resolve from 'resolve-from'
 
 export default {
   input: 'src/index.js',
   plugins: [
     babel(),
-    nodejs(),
+    nodejs({
+      jsnext: true,
+      main: true,
+    }),
     commonjs(),
+    alias({
+      'matches-selector': resolve(__dirname, 'component-matches-selector'),
+      'query': resolve(__dirname, 'component-query'),
+    }),
   ],
 }
--- src/pagination.js
+++ src/pagination.js
@@ -1,6 +1,7 @@
 import EventEmitter from 'eventemitter3'
 import insertCss from 'insert-css'
 import query from 'component-query'
+import closest from 'component-closest'
 import matches from 'component-matches-selector'
 
 export function install (hook, vm) {
@@ -21,24 +22,48 @@ export function install (hook, vm) {
   })
 }
 
+function toArray (elements) {
+  return Array.prototype.slice.call(elements)
+}
+function findHyperlink (li) {
+  return toArray(li.children).find((child) => child.tagName && child.tagName.toUpperCase() === 'A')
+}
+
 function pagination (vm) {
-  const path = vm.route.path
-  const items = Array.prototype.slice.call(query.all('.sidebar li a')).filter((element) => !matches(element, '.section-link'))
-  const active = items.findIndex((link) => link.getAttribute('href') === `#${path}`)
-  return {
-    prev: link(items[active - 1]),
-    next: link(items[active + 1]),
+  try {
+    const path = vm.route.path
+    const all = toArray(query.all('.sidebar li a')).filter((element) => !matches(element, '.section-link'))
+    const active = all.find((item) => item.getAttribute('href') === `#${path}`)
+    const group = toArray((closest(active, 'ul') || {}).children)
+      .filter((element) => element.tagName.toUpperCase() === 'LI')
+    const index = group.findIndex((item) => {
+      const hyperlink = findHyperlink(item)
+      return hyperlink && hyperlink.getAttribute('href') === `#${path}`
+    })
+    return {
+      prev: new Link(group[index - 1]).toJSON(),
+      next: new Link(group[index + 1]).toJSON(),
+    }
+  } catch (error) {
+    return {}
   }
 }
 
-function link (element) {
-  if (!element) {
-    return
+class Link {
+  constructor (element) {
+    if (!element) {
+      return
+    }
+    this.hyperlink = findHyperlink(element)
   }
-  return {
-    name: element.innerText,
-    href: element.getAttribute('href'),
-    _element: element,
+  toJSON () {
+    if (!this.hyperlink) {
+      return
+    }
+    return {
+      name: this.hyperlink.innerText,
+      href: this.hyperlink.getAttribute('href'),
+    }
   }
 }
 
